8641ecea14fd6c2f97075079f772d2e4edbf6994
joaosobrin <jc8576578@gmail.com>
2022-06-29 11:58:51 -0400

    alterado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-
 import "./App.css"
+import "./Plano.jsx"
+import Plano from "./Plano.jsx";
 
 function App() {
-    
+    const palnos = [
+        {
+            title: "Start",
+            price: "Free",
+            qnt_users: "1 user",
+            qnt_projects: "1 project",
+        },
+        {
+            title: "Basic",
+            price: "$19.99",
+            qnt_users: "5 Users",
+            qnt_projects: "20 Projects",
+        },
+        {
+            title: "Expert",
+            price: "$129.99",
+            qnt_users: "Unlimited Users",
+            qnt_projects: "Unlimited Projects",
+        },
+    ];
 
     return (
-        <div className='container'>
-            
-
+        <div className="pai">
+                <Plano />
+                <Plano />
+                <Plano />  
         </div>
 
     );
